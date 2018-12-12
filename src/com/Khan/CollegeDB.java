991bb1de3927d6d3fb99c08f13663878e5567066
nhann <nhanndeh@yahoo.com>
2018-12-12 11:07:42 -0600

    Students table created !

--- src/com/Khan/CollegeDB.java
+++ src/com/Khan/CollegeDB.java
@@ -4,10 +4,12 @@ import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import static sun.plugin.javascript.navig.JSType.URL;
+
 public class CollegeDB {
 
 
-    private static final String DB_CONNECTION_URL = "jdbc:sqlite:college.sqlite";   // Connection String
+     String url = "jdbc:sqlite:college.sqlite";   // Connection String
 
     private static final String TABLE_NAME1 = "students";
     private static final String TABLE_NAME2 = "instructors";
@@ -18,11 +20,11 @@ public class CollegeDB {
 
     public static void main(String[] args) throws SQLException {
 
-        Connection connection = DriverManager.getConnection(DB_CONNECTION_URL);
+        Connection connection = DriverManager.getConnection(URL);
         Statement statement = connection.createStatement();
 
           // Create the course table
-        
+
         String createTableSql = "CREATE TABLE IF NOT EXISTS courses(id INTEGER PRIMARY KEY, courseID TEXT, className TEXT, Instructor TEXT,ClassTime TEXT, Days TEXT, bldgRoom TEXT )";
         statement.execute(createTableSql);
 
@@ -32,7 +34,7 @@ public class CollegeDB {
         String insertDataSql =("INSERT INTO courses(courseID,className,Instructor,ClassTime,Days,bldgRoom) VALUES('ITEC2545','Java Programing','Brian','9:45am-12:30pm','Tu/Wed','T3030')");
         statement.execute(insertDataSql);
 
-        insertDataSql =("INSERT INTO  students(courseID,className,Instructor,ClassTime,Days,bldgRoom) VALUES('ITEC2425','Steve','12:30pm-3:00pm', 'Thu','T300')");
+        insertDataSql =("INSERT INTO  courses (courseID,className,Instructor,ClassTime,Days,bldgRoom) VALUES('ITEC2425','Steve','12:30pm-3:00pm', 'Thu','T300')");
         statement.execute(insertDataSql);
 
         String getAllCoursesSql = "SELECT * FROM courses";
@@ -51,6 +53,42 @@ public class CollegeDB {
 
         }
             allCourses.close();  // close result set
+            statement.close();   // close statement
+            connection.close(); // Close connection
+
+
+                   // Create students table
+
+         createTableSql = "CREATE TABLE IF NOT EXISTS students (id INTEGER PRIMARY KEY, studentID TEXT, studentName TEXT,className TEXT, Instructor TEXT,ClassTime TEXT, Days TEXT, bldgRoom TEXT )";
+         statement.execute(createTableSql);
+                  // Populate student table with data
+
+         insertDataSql = "INSERT INTO students(studentID,studentName, className,Instructor,ClassTime,Days,bldgRoom) VALUES('ITEC2545','Java Programing','Paul Mark','Brian','9:45am-12:30pm','Tu/Wed','T3030')";
+         statement.execute(insertDataSql);
+         insertDataSql = "INSERT INTO students (studentID,studentName, className,Instructor,ClassTime,Days,bldgRoom) VALUES('ITEC2545','Java Programing','Henriette Marie','Brian','9:45am-12:30pm','Tu/Wed','T3030')";
+         statement.execute(insertDataSql);
+
+         String getAllStudentsSql = "SELECT * FROM students";
+         ResultSet allStudents = statement.executeQuery(getAllStudentsSql);
+
+                    // Loop over the result set
+         while(allStudents.next()){
+
+             String studentID = allStudents.getString("StudentID");
+             String studentName = allStudents.getString("studentName");
+             String courseName = allStudents.getString("className");
+             String professor = allStudents.getString("Instructor");
+             String time = allStudents.getString("classTime");
+             String days = allStudents.getString("Days");
+             String hallNumber = allStudents.getString("bldgRoom");
+
+             System.out.printf("%s %s has %s at %s on %s in %s and the %s name is \n", studentID, studentName,courseName, time, days, hallNumber );
+
+         }
+
+            allStudents.close();  // Close result set
+            statement.close();    // Close statement
+            connection.close();  // Close connection
 
     }
 }
